d32a7988b57ae5bc48909e322306bea9b5704ef4
DueroBone <90635383+DueroBone@users.noreply.github.com>
2023-10-06 17:37:44 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,49 @@
-// create a web server
-const http = require('http');
-
-const hostname = '127.0.0.1';
+// Create a web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+// Use the body parser to get data from the body of POST requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Create a global array to store comments
+const comments = [];
+
+// Add a comment to the global array
+function addComment(comment) {
+    comments.push(comment);
+}
+
+// Return all the comments
+function getComments() {
+    return comments;
+}
+
+// Delete all the comments
+function deleteComments() {
+    comments.length = 0;
+}
+
+// Return all the comments in JSON format
+app.get('/comments', (req, res) => res.json(getComments()));
+
+// Add a comment
+app.post('/comments', (req, res) => {
+    const comment = req.body.comment;
+    addComment(comment);
+    res.json(comment);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// Delete all the comments
+app.delete('/comments', (req, res) => {
+    deleteComments();
+    res.json('All comments deleted');
 });
